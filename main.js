5a86470904cbc9af5f594d3b8fdf3e9bdbe6792a
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-08-03 12:45:15 +0500

    feat: add delet card

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-let btnCreate = document.getElementById('create-btn')
-let toDoList = document.querySelector('.to-do-list')
+const btnCreate = document.getElementById('create-btn')
+const toDoList = document.querySelector('.to-do-list')
+const btnDone = document.querySelector('.btn-done')
+const btnDelet = document.querySelector('.btn-delet')
 
 
 
@@ -9,9 +11,13 @@ btnCreate.addEventListener("click", function(){
     card.className = "to-do-card";
     card.innerHTML = `<span>${inputValue}</span>             
                         <div class="btn">
-                            <button>Done</button>
-                            <button>Delete</button>
+                            <button class="btn-done">Done</button>
+                            <button class="btn-delet">Delete</button>
                         </div>`
     if (inputValue.trim().length !== 0) toDoList.append(card);
 });
 
+
+btnDelet.addEventListener("click", function(){ 
+    btnDelet.parentElement.parentElement.remove()
+});
